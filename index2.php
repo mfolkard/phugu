2b1f7fea3e8033a030fab4f1a4b339edfd6dc1a3
unknown <William Gu@.(none)>
2011-02-20 11:50:14 +0000

    first commit

--- index2.php
+++ index2.php
@@ -4,6 +4,6 @@
 	$var = $_REQUEST['session']->getSessionVar("bob");
 	echo "session value:" . $var . " - the end<br />";
 	$_REQUEST['session']->setSessionVar("bob", "mamakakakakamaka");
-
+	print "heello";
 	include("inc/footer.php");
 ?>
